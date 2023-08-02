7496ec61dfd2f59e6f902ee75724fb1042eba2b1
nstonic <118763806+nstonic@users.noreply.github.com>
2023-08-02 18:47:17 +0400

    Delete posts from UserSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -8,7 +8,7 @@ from users.models import CustomUser
 class UserSerializer(ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'posts']
+        fields = ['username', 'email']
 
 
 class PostSerializer(ModelSerializer):
