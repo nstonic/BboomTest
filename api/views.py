6e00c01b8e9d59945a4f7e9b80080904a5af7a0e
nstonic <nstonic@gmail.com>
2023-08-03 10:53:27 +0400

    Add permition control

--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, viewsets, permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
@@ -14,6 +14,7 @@ class PostViewSet(
 ):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 
 class UserViewSet(
